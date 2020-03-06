5e651ab1312352fceb6b4fc7307de66e268cd1ee
alllef <alekz29@gmail.com>
2020-03-07 01:38:37 +0200

    added demo version of initializing team names and results from file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+
 using namespace std;
 
+void initializeResults(vector<int> &team, vector<int> &opponent, vector<string> &nameOfTheTeam, int &teams);
+
 int main() {
+    vector<int> resultsOfTeam = {};
+    vector<int> resultsOfOpponent = {};
+    vector<string> nameOfTeam;
+    int numberOfTeams = 0;
     return 0;
 }
+
+void initializeResults(vector<int> &team, vector<int> &opponent, vector<string> &nameOfTheTeam, int &teams) {
+
+    ifstream inFile;
+    int counter = 0;
+    int opponentNumber = 0;
+    int teamNumber = 0;
+
+    string teamName;
+    inFile.open("premier_league1.csv");
+    if (!inFile.is_open()) cout << "It is not open" << endl;
+    inFile >> teams;
+
+    while (!inFile.eof()) {
+        while (inFile.get() != '/n') {
+            while (inFile.get() != ',') teamName += inFile.get();
+            nameOfTheTeam.push_back(teamName);
+
+            if (counter % 2 == 0) {
+                inFile >> teamNumber;
+                team.push_back(teamNumber);
+            } else {
+                inFile >> opponentNumber;
+                opponent.push_back(opponentNumber);
+            }
+            counter++;
+        }
+
+    }
+    inFile.close();
+}
+
