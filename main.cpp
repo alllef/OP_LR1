17bf1a0a351c042c8b19f74bef233ca33f84e65c
alllef <alekz29@gmail.com>
2020-03-09 15:35:26 +0200

    added sort and initializing finally works

--- main.cpp
+++ main.cpp
@@ -5,43 +5,48 @@
 
 using namespace std;
 
-void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> results);
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results);
+void sortTeams(vector<string> &nameOfTheTeam, int &teams, vector<int> &results);
 
 int main() {
 
     vector<string> teamName;
     int teamAmount = 0;
     vector<int> teamResults;
-    initializeResults(teamName, teamAmount, teamResults);
 
+    initializeResults(teamName, teamAmount, teamResults);
+    sortTeams(teamName, teamAmount, teamResults);
     return 0;
 }
 
-void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> results) {
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results) {
 
-    ifstream inFile;
+    fstream inFile;
     int score = 0;
-    int opponentNumber = 0;
-    int teamNumber = 0;
+    int opponentNumber;
+    int teamNumber;
     char c = 0;
-    string teamName = "";
+    string teamName;
 
     inFile.open(R"(C:\Users\alekz29\Documents\GitHub\OP_LR1\premier_league1.csv)");
     if (!inFile.is_open()) cout << "It is not open" << endl;
+
     inFile >> teams;
     inFile.get(c);
+
     for (int i = 0; i < teams; i++) {
         inFile.get(c);
+        string fullName;
         while (c != ',') {
-             teamName += c;
+            fullName += c;
             inFile.get(c);
         }
+        nameOfTheTeam.push_back(fullName);
 
-
-        while (c != '\n' && inFile.eof()) {
-
+        while (c != '\n' && !inFile.eof()) {
             inFile >> teamNumber;
             inFile.get(c);
+
             inFile >> opponentNumber;
             if (teamNumber > opponentNumber)
                 score += 3;
@@ -49,17 +54,34 @@ void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> re
                 score += 1;
             inFile.get(c);
         }
-        nameOfTheTeam.push_back(teamName);
-        teamName="";
+
+
         results.push_back(score);
-        score=0;
+        score = 0;
     }
 
 
     inFile.close();
 }
 
+void sortTeams(vector<string> &nameOfTheTeam, int &teams, vector<int> &results) {
 
+    string name;
+    int result;
 
+    for (int i = 0; i < teams; i++) {
+        for (int j = 0; j < teams-1; j++) {
+            if (results[j] > results[j + 1]) {
+                result = results[j];
+                results[j] = results[j + 1];
+                results[j + 1] = result;
 
+                name = nameOfTheTeam[j];
+                nameOfTheTeam[j] = nameOfTheTeam[j + 1];
+                nameOfTheTeam[j + 1] = name;
+            }
+        }
+    }
+}
 
+void ()
