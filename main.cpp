0810533cde5dc56b2f219530b017b08bfc26e2d8
alllef <alekz29@gmail.com>
2020-03-09 11:52:56 +0200

    why it's not working?

--- main.cpp
+++ main.cpp
@@ -1,47 +1,65 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include<string>
 
 using namespace std;
 
-void initializeResults(vector<int> &team, vector<int> &opponent, vector<string> &nameOfTheTeam, int &teams);
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> results);
 
 int main() {
-    vector<int> resultsOfTeam = {};
-    vector<int> resultsOfOpponent = {};
-    vector<string> nameOfTeam;
-    int numberOfTeams = 0;
+
+    vector<string> teamName;
+    int teamAmount = 0;
+    vector<int> teamResults;
+    initializeResults(teamName, teamAmount, teamResults);
+
     return 0;
 }
 
-void initializeResults(vector<int> &team, vector<int> &opponent, vector<string> &nameOfTheTeam, int &teams) {
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> results) {
 
     ifstream inFile;
-    int counter = 0;
+    int score = 0;
     int opponentNumber = 0;
     int teamNumber = 0;
+    char c = 0;
+    string teamName = "";
 
-    string teamName;
-    inFile.open("premier_league1.csv");
+    inFile.open(R"(C:\Users\alekz29\Documents\GitHub\OP_LR1\premier_league1.csv)");
     if (!inFile.is_open()) cout << "It is not open" << endl;
     inFile >> teams;
-
-    while (!inFile.eof()) {
-        while (inFile.get() != '/n') {
-            while (inFile.get() != ',') teamName += inFile.get();
-            nameOfTheTeam.push_back(teamName);
-
-            if (counter % 2 == 0) {
-                inFile >> teamNumber;
-                team.push_back(teamNumber);
-            } else {
-                inFile >> opponentNumber;
-                opponent.push_back(opponentNumber);
-            }
-            counter++;
+    inFile.get(c);
+    for (int i = 0; i < teams; i++) {
+        inFile.get(c);
+        while (c != ',') {
+             teamName += c;
+            inFile.get(c);
         }
 
+
+        while (c != '\n' && inFile.eof()) {
+
+            inFile >> teamNumber;
+            inFile.get(c);
+            inFile >> opponentNumber;
+            if (teamNumber > opponentNumber)
+                score += 3;
+            else if (teamNumber == opponentNumber)
+                score += 1;
+            inFile.get(c);
+        }
+        nameOfTheTeam.push_back(teamName);
+        teamName="";
+        results.push_back(score);
+        score=0;
     }
+
+
     inFile.close();
 }
+
+
+
+
 
