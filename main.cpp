32972216844e91d379626b85247b0de16a60c460
alllef <alekz29@gmail.com>
2020-03-10 23:34:10 +0200

    added working with several files

--- main.cpp
+++ main.cpp
@@ -2,32 +2,38 @@
 #include <fstream>
 #include <vector>
 #include<string>
-#include <filesystem>
 #include <Windows.h>
 
 using namespace std;
 
-void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results);
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results, vector<string> &file, int &number);
 
 void sortTeams(vector<string> &nameOfTheTeam, int &teams, vector<int> &results);
+
 
 void noteResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results);
 
 void makeMenu(vector<string> &name, int &number);
+
 int main() {
+
+    SetConsoleOutputCP(CP_UTF8);
     vector<string> fileName;
     vector<string> teamName;
     int teamAmount = 0;
     vector<int> teamResults;
+    int fileNumber;
 
-    initializeResults(teamName, teamAmount, teamResults);
+    makeMenu(fileName, fileNumber);
+    initializeResults(teamName, teamAmount, teamResults, fileName, fileNumber);
     sortTeams(teamName, teamAmount, teamResults);
     noteResults(teamName, teamAmount, teamResults);
 
     return 0;
 }
 
-void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results) {
+
+void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &results, vector<string> &file, int &number) {
 
     fstream inFile;
     int score = 0;
@@ -35,41 +41,52 @@ void initializeResults(vector<string> &nameOfTheTeam, int &teams, vector<int> &r
     int teamNumber;
     char c = 0;
     string teamName;
+int totalTeamNumber=0;
 
-    inFile.open();
+for(int i=0; i<number; i++) {
+    inFile.open(file[i]);
     if (!inFile.is_open()) cout << "It is not open" << endl;
 
-    inFile >> teams;
+    inFile >> totalTeamNumber;
+    teams+=totalTeamNumber;
     inFile.get(c);
 
-    for (int i = 0; i < teams; i++) {
+    for (int j = 0; j < teams; j++) {
         inFile.get(c);
         string fullName;
         while (c != ',') {
             fullName += c;
+            teamName += c;
             inFile.get(c);
         }
         nameOfTheTeam.push_back(fullName);
 
-        while (c != '\n' && !inFile.eof()) {
-            inFile >> teamNumber;
-            inFile.get(c);
 
-            inFile >> opponentNumber;
-            if (teamNumber > opponentNumber)
-                score += 3;
-            else if (teamNumber == opponentNumber)
-                score += 1;
-            inFile.get(c);
+        while (c != '\n' && !inFile.eof()) {
+            while (c != '\n' && inFile.eof()) {
+
+                inFile >> teamNumber;
+                inFile.get(c);
+
+                inFile >> opponentNumber;
+                if (teamNumber > opponentNumber)
+                    score += 3;
+                else if (teamNumber == opponentNumber)
+                    score += 1;
+                inFile.get(c);
+            }
+
+
+            nameOfTheTeam.push_back(teamName);
+            teamName = "";
+            results.push_back(score);
+            score = 0;
         }
 
 
-        results.push_back(score);
-        score = 0;
+        inFile.close();
     }
-
-
-    inFile.close();
+}
 }
 
 void sortTeams(vector<string> &nameOfTheTeam, int &teams, vector<int> &results) {
@@ -106,8 +123,8 @@ void makeMenu(vector<string> &name, int &number) {
     cout << "Введите количество файлов:\n";
     cin >> number;
     for (int i = 0; i < number; i++) {
-        cout<<"Введите полное название файла:\n";
-        cin>>fileName;
+        cout << "Введите полное название файла:\n";
+        cin >> fileName;
         name.push_back(fileName);
     }
 }
